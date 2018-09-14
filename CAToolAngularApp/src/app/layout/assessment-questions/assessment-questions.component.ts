7a7a5a806e9d26952b484e3fc878f2475b17f604
jkomal10 <komal.a.jagtap@capgemini.com>
2018-09-14 11:21:16 +0530

    data table

--- CAToolAngularApp/src/app/layout/assessment-questions/assessment-questions.component.ts
+++ CAToolAngularApp/src/app/layout/assessment-questions/assessment-questions.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assessment-questions',
   templateUrl: './assessment-questions.component.html',
-  styleUrls: ['./assessment-questions.component.scss']
+  styleUrls: ['./assessment-questions.component.scss'],
 })
 export class AssessmentQuestionsComponent implements OnInit {
+  password : string = "pass";
+  username : string = "user";
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
+  onLoggedin() {
+    console.log('add page');
+    localStorage.setItem('isLoggedin', 'true');   
+    if("pass"==this.password)
+    {
+       this.router.navigate(['/dashboard']);
+       console.log(this.username);
+    }    
+   //  this.loginservice.getUserByID(formValues.username).subscribe((data) => {
+   //     this.user = data;
+   //     if(this.user.password != null){
+   //         if(formValues.password == this.user.password){
+   //             console.log(this.user.password);
+   //             this.router.navigate(['/dashboard']);
+   //         }
+           else{
+                alert("invalid Password");
+           }
+       }
+       // else{
+       //     alert("invalid UserName");
+       // }});
+
+   //this.router.navigate(['/dashboard']);
+//}
 }
--- CAToolAngularApp/src/app/layout/assessment-questions/assessment-questions.module.ts
+++ CAToolAngularApp/src/app/layout/assessment-questions/assessment-questions.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataTablesModule } from 'angular-datatables';
+import { FormsModule } from '@angular/forms';
 
 import { AssessmentQuestionsRoutingModule } from './assessment-questions-routing.module';
 import { AssessmentQuestionsComponent } from './assessment-questions.component';
 
 @NgModule({
-    imports: [CommonModule, AssessmentQuestionsRoutingModule],
+    imports: [CommonModule, AssessmentQuestionsRoutingModule,DataTablesModule],
     declarations: [AssessmentQuestionsComponent]
 })
 
