daecce612ed499aeac0b0a0bb90c819f245991ff
jkomal10 <komal.a.jagtap@capgemini.com>
2018-10-04 15:18:00 +0530

    no of optiona

--- CAToolAngularApp/src/app/layout/assessment-questions/add-assessment-question/add-assessment-question.component.ts
+++ CAToolAngularApp/src/app/layout/assessment-questions/add-assessment-question/add-assessment-question.component.ts
@@ -10,12 +10,52 @@ import { AssessmentQuestionsService } from '../assessment-questions.service';
 })
 export class AddAssessmentQuestionComponent implements OnInit {
 
+  answerValues : string;
+  Options : Array<number>=[10];
+  //Options : number[];
   question: Question = new Question();
   submitted = false;
-
+  numberOfOptions : number;
+  optionsValues = [1, 2, 3,4,5,6,7,8,9,10];
+  
+  
   constructor(private questionService: AssessmentQuestionsService,public router: Router) { }
 
   ngOnInit() {
+
+   
+  }
+
+  selectChangeHandler(event:any){
+    console.log(event);
+    this.numberOfOptions=parseInt(event.target.value,10);
+   // this.numberOfOption=event;
+    console.log(this.numberOfOptions);
+    console.log(event.target.value);
+   // this.numberOfOption1 = Array.of(this.numberOfOptions);
+    //console.log(this.numberOfOption1);
+   // this.Options[event];
+   //  console.log(this.Options);
+    //console.log(this.Options);
+    for (let index = 1; index <= this.numberOfOptions ; index++) {
+      console.log(index);
+       this.Options[index] = index;
+       console.log(this.Options);
+       console.log(this.Options.length);
+    }
+    // for (const key in event) {
+    //   if (event.hasOwnProperty(key)) {
+    //     const element = event[key];
+    //     console.log(element)  ;
+    //   }
+    // }
+    // console.log(this.Options);
+  }
+
+  options(){
+    for (let index = 0; index < this.question.numberOfOption ; index++) {
+      this.Options[index] = index; 
+    }
   }
 
   newQuestion(): void {
@@ -30,7 +70,7 @@ export class AddAssessmentQuestionComponent implements OnInit {
   }
 
   onSubmit() {
-    this.submitted = true;
+    this.submitted = true
     this.save();
   }
 
