275e9e83080dee34e2ba5ab46190275993ce9d61
jkomal10 <komal.a.jagtap@capgemini.com>
2018-10-25 22:25:41 +0530

    cloud provider

--- CATToolApp/src/main/java/com/cattool/application/controller/CloudProviderController.java
+++ CATToolApp/src/main/java/com/cattool/application/controller/CloudProviderController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cattool.application.entity.AssessmentQuestions;
 import com.cattool.application.entity.CloudProvider;
+import com.cattool.application.service.AssessmentQuestionsService;
 import com.cattool.application.service.CloudProviderService;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -25,6 +28,13 @@ public class CloudProviderController {
 	public List<CloudProvider> getCloudProvider()
 	{
 		return cloudProviderService.getAllcloudProvider();
+	}
+	
+	@GetMapping("/getAllCloudProviderQuestion/{cloudProviderId}")
+	public List<AssessmentQuestions> getCloudProviderQuestion(@PathVariable int cloudProviderId)
+	{
+		System.out.println("**********getAllCloudProviderQuestion*********");
+		return cloudProviderService.getCloudProviderQuestion(cloudProviderId);
 	}
 	
 	@PutMapping("/setEvaluationOrder")
