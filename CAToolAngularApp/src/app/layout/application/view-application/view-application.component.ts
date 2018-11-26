cb4c48ed6102123f0fd2c24fe70eae44a53243d6
jkomal10 <komal.a.jagtap@capgemini.com>
2018-11-26 18:45:25 +0530

    application

--- CAToolAngularApp/src/app/layout/application/view-application/view-application.component.ts
+++ CAToolAngularApp/src/app/layout/application/view-application/view-application.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit,Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ViewApplicationService } from './view-application.service';
-import { Application } from '../Application';
 import { ApplicationService } from '../application.service';
-//import { ApplicationComponent } from '../application.component';
-//import { ApplicationService } from '../application.service';
-//@Input() application: Application;
 class DataTablesResponse {
   data: any[];
   draw: number;
@@ -23,27 +18,15 @@ export class ViewApplicationComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
   AllData : any = [];
-  //Application AllData =new Application();
-  //id=11;
   message:String;
   constructor(private router:Router, private http:HttpClient,private applicationService:ApplicationService) { }
- //constructor(private getData:ApplicationService,private app:ApplicationComponent){}
   ngOnInit() {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 2,
       responsive: true};
-  
-       //ViewApplicationById(formvalues){
-      // this.getData.CollectSingleApplicationData(this.id).subscribe(result => 
-      //   {
-      //   this.AllData = result ;
-      //   this.dtTrigger.next();
-      //   console.log(this.AllData);
-      //   });
-
         this.applicationService.question.subscribe(data => this.message = data); 
   }
-//}
+
 
 }
